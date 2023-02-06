abe05e83e14b3862437b37262253f8328e28851d
Rohit Agarwal <>
2023-02-06 17:40:35 +0100

    config file

---
+++ config.py
@@ -0,0 +1,18 @@
+class Args:
+
+    # Data Loading
+    # ============
+
+    batch_size = 32
+    num_workers = 4
+
+    # Regularization
+    # ==============
+    dropout = 0.1
+
+    # Training
+    # ========
+    random_seed = 1
+    epochs = 100
+    learning_rate = 0.01
+    momentum = 0.9
